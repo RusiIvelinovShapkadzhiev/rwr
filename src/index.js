ab77e7db5afecbe35cca63f9edaba3e8250d415e
unknown <dimitria_d@abv.bg>
2020-11-16 09:10:49 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = function(){
+    return <div>Hi there!</div>
+}
+
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
+);
